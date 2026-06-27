8fb5079cc884d26f50ccb05994dab02a22e923d6
Kristen <kristen4810@gmail.com>
2026-06-27 22:06:05 +0100

    Add player movement via input keys

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
+        .add_systems(Update, move_player)
         .run();
 }
 
@@ -24,4 +25,31 @@ fn setup(mut commands: Commands) {
             Transform::from_translation(Vec3::ZERO),
             Player,
     ));
+}
+
+fn move_player(
+    input: Res<ButtonInput<KeyCode>>,
+    time: Res<Time>,
+    mut player_transform: Single<&mut Transform, With<Player>>,
+) {
+    let mut direction = Vec2::ZERO;
+    if input.pressed(KeyCode::ArrowLeft) {
+        direction.x -= 1.0;
+    }
+    if input.pressed(KeyCode::ArrowRight) {
+        direction.x += 1.0;
+    }
+    if input.pressed(KeyCode::ArrowUp) {
+        direction.y += 1.0;
+    }
+    if input.pressed(KeyCode::ArrowDown) {
+        direction.y -= 1.0;
+    }
+
+    if direction != Vec2::ZERO {
+        let speed = 300.0;
+        let delta = direction.normalize() * speed * time.delta_secs();
+        player_transform.translation.x += delta.x;
+        player_transform.translation.y += delta.y;
+    }
 }
