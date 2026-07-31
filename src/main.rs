a8a76d4be0f340ff66df23d91788d5b5e9386938
Kristen <kristen4810@gmail.com>
2026-07-31 21:11:58 +0100

    Use tilemap generator on startup + add tilemap

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,39 @@
-use bevy::prelude::*;
-use crate::player::PlayerPlugin;
-
+mod map;
 mod player;
 
+use bevy::{
+    prelude::*,
+    window::{Window, WindowPlugin, WindowResolution},
+};
+
+use bevy_procedural_tilemaps::prelude::*;
+
+use crate::map::generate::{map_pixel_dimensions, setup_generator};
+use crate::player::PlayerPlugin;
+
 fn main() {
+    let map_size = map_pixel_dimensions();
+
     App::new()
         .insert_resource(ClearColor(Color::WHITE))
         .add_plugins(
             DefaultPlugins
-            .set(ImagePlugin::default_nearest())
-            .set(AssetPlugin {
+                .set(AssetPlugin {
                 file_path: "src/assets".into(),
                 ..default()
-            }),
+                })
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        resolution: WindowResolution::new(map_size.x as u32, map_size.y as u32),
+                        resizable: false,
+                        ..default()
+                    }),
+                    ..default()
+                })
+                .set(ImagePlugin::default_nearest()),
         )
-        .add_systems(Startup, setup_camera)
+        .add_plugins(ProcGenSimplePlugin::<Cartesian3D, Sprite>::default())
+        .add_systems(Startup, (setup_camera, setup_generator))
         .add_plugins(PlayerPlugin)
         .run();
 }
@@ -28,4 +47,3 @@ fn setup_camera(mut commands: Commands) {
             }),
     ));
 }
-
